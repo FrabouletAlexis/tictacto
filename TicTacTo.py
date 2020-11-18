9acc79ffe925776a2ae395e03b41d379ace50ac8
FrabouletAlexis <a.fraboulet@rns.ecolescreatives.com>
2020-11-18 17:59:10 +0100

    Update TicTacTo.py

--- TicTacTo.py
+++ TicTacTo.py
@@ -8,13 +8,13 @@ def afficheGrille (grille):
         print(grille[3*i],grille[3*i+1],grille[3*i+2],"\n")
 
 def ajouteSymbole(grille,joueur):
-    choixIncorrect=True
-    while( choixIncorrect=True):
+    choixIncorrect==True
+    while( choixIncorrect==True):
         i= int(input("Sur quelle ligne voulez-vous jouer ?"))
         j= int(input("Sur quelle colonne voulez-vous ?"))
         if (grille[3*i+j]!=" "):
             grille [3*i+j] = joueur
-            choixIncorrect=False
+            choixIncorrect==False
 
             
 input()

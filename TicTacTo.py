a72451f0d9a58fb05db5220f89a27b50bcac3aa8
FrabouletAlexis <a.fraboulet@rns.ecolescreatives.com>
2020-11-18 17:35:30 +0100

    Update TicTacTo.py

--- TicTacTo.py
+++ TicTacTo.py
@@ -1,3 +1,9 @@
+
 def initialiseGrille(grille):
     for compteur in range (0,8):
-        grille[compteur]=""
+        grille[compteur]=" "
+
+def afficheGrille (grille):
+    for i in range (0,2):
+        print(grille[3*i],grille[3*i+1],grille[3*i+2],"\n")
+input()
